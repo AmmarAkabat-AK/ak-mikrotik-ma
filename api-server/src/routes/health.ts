0d3cd81882affbf40b7842c1a7064e0eceb28de1
AmmarAkabat-AK <ammar.akabat@gmail.com>
2026-05-11 02:55:42 +0300

    Add files via upload

---
+++ api-server/src/routes/health.ts
@@ -0,0 +1,13 @@
+
+import { Router, type IRouter } from "express";
+
+const router: IRouter = Router();
+
+router.get("/healthz", (_req, res) => {
+  res.json({
+    status: "ok"
+  });
+});
+
+export default router;
+
